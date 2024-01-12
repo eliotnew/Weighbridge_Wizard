a4df9a5ec6733f3c44a2cac2997f080d3b10fb26
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-01-12 00:15:29 +0000

    added themes and simple app bar to test

--- ww-react-app/src/App.jsx
+++ ww-react-app/src/App.jsx
@@ -1,13 +1,32 @@
+import React, { useContext } from "react";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, ThemeContext } from "./themes/ThemeContext";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landing-page/LandingPage";
 import "./App.css";
 
 function App() {
+  const { currentTheme, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
+    <div>
+      <button onClick={toggleTheme} style={{ backgroundColor: "grey" }}>
+        Toggle Theme
+      </button>
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+export default () => (
+  <ThemeProvider>
+    {(theme) => (
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    )}
+  </ThemeProvider>
+);
--- ww-react-app/src/components/landing-page/LPappbar.jsx
+++ ww-react-app/src/components/landing-page/LPappbar.jsx
@@ -5,15 +5,93 @@ import { faHatWizard } from "@fortawesome/free-solid-svg-icons";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 function LPappbar() {
+  const theme = useTheme();
   return (
     <AppBar
       position="static"
       sx={{
+        backgroundColor: theme.palette.primary.main,
         boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.2)",
-        backgroundColor: "blue",
       }}
     >
-      hello
+      <Toolbar>
+        <FontAwesomeIcon
+          icon={faHatWizard}
+          size="2xl"
+          flip="horizontal"
+          style={{ color: theme.palette.text.primary }}
+        />
+        <Typography
+          className="WizardFont"
+          variant="h4"
+          noWrap
+          component="div"
+          sx={{
+            fontFamily: "sans-serif",
+            paddingLeft: "10px",
+            display: { xs: "none", sm: "block" },
+          }}
+          style={{
+            fontFamily: "Bona Nova",
+            color: theme.palette.primary.contrastText,
+            fontWeight: 1000,
+          }}
+        >
+          WeighBridge Wizard
+        </Typography>
+        <Typography
+          variant="h4"
+          component="div"
+          sx={{ flexGrow: 1, paddingLeft: "12px" }}
+        ></Typography>
+        <Button
+          sx={{
+            color: theme.palette.primary.contrastText,
+            fontWeight: 1000,
+            "&:hover": {
+              backgroundColor: theme.palette.primary.contrastText,
+              color: theme.palette.primary.main,
+              transform: "scale(1.1)",
+              transition: "transform 0.35s", // Change background colour on hover
+            },
+          }}
+          color="inherit"
+        >
+          About
+        </Button>
+        <Typography
+          variant="h6"
+          component="span"
+          style={{
+            margin: "0 8px",
+            marginBottom: "5px",
+            color: theme.palette.primary.contrastText,
+            fontWeight: 1000,
+          }}
+        >
+          |
+        </Typography>
+        <Button
+          sx={{
+            color: theme.palette.primary.contrastText,
+            fontWeight: 1000,
+            "&:hover": {
+              backgroundColor: theme.palette.primary.contrastText,
+              color: theme.palette.primary.main,
+              transform: "scale(1.1)",
+              transition: "transform 0.35s", // Change background colour on hover
+            },
+          }}
+          color="inherit"
+        >
+          Sign In{" "}
+          <FontAwesomeIcon
+            icon={faRightToBracket}
+            beatFade
+            style={{ marginLeft: "6px" }}
+          />
+        </Button>
+      </Toolbar>
     </AppBar>
   );
 }
