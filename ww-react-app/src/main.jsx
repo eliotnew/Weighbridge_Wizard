9b33f6961dacfd4ea6a5b3a5a6cef23793732dba
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-01-15 17:59:45 +0000

    Imported CSS content from old proejct

--- ww-react-app/src/main.jsx
+++ ww-react-app/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
+/**
+ * This document is comparable to the "index.js" found in a create-react app (this is a vite react app)
+ */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
