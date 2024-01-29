10d0b5046bcc465f79e88124c6a43960d6c0318b
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-01-29 14:20:47 +0000

    Finished adding unique content components

---
+++ ww-react-app/src/components/dashboard/tab-content/TrainingContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TrainingContent() {
+  const num = Math.floor(Math.random() * 10000);
+  return <>Training Content {num}</>;
+}
+export default TrainingContent;
---
+++ ww-react-app/src/components/dashboard/tab-content/ViewClosedOrdersContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ViewClosedOrdersContent() {
+  const num = Math.floor(Math.random() * 10000);
+  return <>View Closed Orders Content {num}</>;
+}
+export default ViewClosedOrdersContent;
