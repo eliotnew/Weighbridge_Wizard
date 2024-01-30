95332b0ba6cf4ca62425082c25a285dc27a939cf
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-01-30 17:10:56 +0000

    Added server code and adapted it from fullstack module project for me to work on

--- ww-express-app/server.js
+++ ww-express-app/server.js
@@ -12,7 +12,7 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 //---------------------------------------------------Set up the express app
 const app = express();
 app.use(express.json());
-const port = 3001;
+const port = 3060;
 
 // cross origins resource sharing policy set up to only accept from local host (ie the same as server). This should be sufficient for this CW.
 app.use(cors());
@@ -22,3 +22,63 @@ app.use(cors());
 //I will add the routes later
 
 // ---------------------------------------------------Connect to MongoDB
+async function connectToDatabase() {
+  try {
+    const useInMemoryDB = process.env.NODE_ENV === "test";
+    const realDbConnection = "mongodb://mongodb:27017/weighbridge-wizard";
+
+    if (useInMemoryDB) {
+      //test environment
+      const mongoMemoryServer = await MongoMemoryServer.create();
+      const uri = mongoMemoryServer.getUri();
+
+      await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to in-memory MongoDB!");
+    } else {
+      //non test environment
+      await mongoose.connect(realDbConnection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+    }
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+}
+
+connectToDatabase();
+
+//---------------------------------------------------WebSocket code
+
+const server = require("http").Server(app);
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`'A client Connected: ${socket.id}`);
+
+  socket.emit("message", "Hello ww-client from ww-server via websocket!");
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
+  });
+});
+
+//---------------------------------------------------Start the server.
+server.listen(port, () => {
+  console.log(
+    `Weighbridge Wizard's server is running on http://localhost:${port}`
+  );
+});
+
+//module.exports = server;????
