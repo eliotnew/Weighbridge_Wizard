816ec73c793de7a303cfeeea50d82d55753e6fcd
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-02 12:11:17 +0000

    added a get all tickets route

---
+++ ww-express-app/routes/ticket-routes/getAllTickets.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const ticketModel = require("../../models/ticketModel");
+
+// returns all onsite trucks
+router.get("/", async (req, res) => {
+  try {
+    const tickets = await ticketModel.find({ onsite: true });
+    res.status(200).json(tickets);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
