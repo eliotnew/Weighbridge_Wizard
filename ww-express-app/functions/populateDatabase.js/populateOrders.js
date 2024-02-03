f3fb225ba97ffaa37a8aeb50328b4de56e0e1f78
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-03 12:57:38 +0000

    populate orders finito

--- ww-express-app/functions/populateDatabase.js/populateOrders.js
+++ ww-express-app/functions/populateDatabase.js/populateOrders.js
@@ -6,29 +6,28 @@ const generateOrderNumberID = require("../generateOrderNumberID");
  */
 async function populateOrders() {
   try {
-    const camelotProducts = [
-      { product: "Limestone 6mm", compatible: "dump", location: "camelot" },
-      { product: "Limestone 10mm", compatible: "dump", location: "camelot" },
-      { product: "Limestone 20mm", compatible: "dump", location: "camelot" },
-      { product: "Fine Sand 0/4mm", compatible: "dump", location: "camelot" },
-      { product: "Clay Pellets", compatible: "dump", location: "camelot" },
-      { product: "Animal Feed", compatible: "dump", location: "camelot" },
-      { product: "Asphalt mix 1", compatible: "hotbox", location: "camelot" },
-      { product: "Asphalt mix 2", compatible: "hotbox", location: "camelot" },
-      {
-        product: "Shipping Container",
-        compatible: "sidelifter",
-        location: "camelot",
-      },
-    ];
+    //---------------------------------------------------> Create ids
     const id1 = generateOrderNumberID();
-    const id2 = generateOrderNumberID();
-    const id3 = generateOrderNumberID();
-    const id4 = generateOrderNumberID();
-    const id5 = generateOrderNumberID();
-    const id6 = generateOrderNumberID();
+    const exists1 = await orderModel.findOne({ orderNumber: id1 });
 
-    const closedOrder1 = {
+    const id2 = generateOrderNumberID();
+    const exists2 = await orderModel.findOne({ orderNumber: id2 });
+
+    const id3 = generateOrderNumberID();
+    const exists3 = await orderModel.findOne({ orderNumber: id3 });
+
+    const id4 = generateOrderNumberID();
+    const exists4 = await orderModel.findOne({ orderNumber: id4 });
+
+    const id5 = generateOrderNumberID();
+    const exists5 = await orderModel.findOne({ orderNumber: id5 });
+
+    const id6 = generateOrderNumberID();
+    const exists6 = await orderModel.findOne({ orderNumber: id6 });
+
+    //---------------------------------------------------> Create Entities
+
+    const closedOrder1 = new orderModel({
       orderNumber: id1,
       company: "Smith & Sons Tarmac",
       dateStart: "04/10/2023",
@@ -43,8 +42,8 @@ async function populateOrders() {
       deliveryPostCode: "PL36YE",
       contactPhone: 445512439303,
       contactEmail: "smith.tarmac@email.com",
-    };
-    const closedOrder2 = {
+    });
+    const closedOrder2 = new orderModel({
       orderNumber: id2,
       company: "Ace Construction",
       dateStart: "08/10/2023",
@@ -59,8 +58,8 @@ async function populateOrders() {
       deliveryPostCode: "FV31DA",
       contactPhone: 447390796360,
       contactEmail: "ace.construction@email.com",
-    };
-    const openOrder1 = {
+    });
+    const openOrder1 = new orderModel({
       orderNumber: id3,
       company: "Smith & Sons Tarmac",
       dateStart: "05/01/2024",
@@ -75,8 +74,8 @@ async function populateOrders() {
       deliveryPostCode: "FV11GT",
       contactPhone: 447390796360,
       contactEmail: "ace.construction@email.com",
-    };
-    const openOrder2 = {
+    });
+    const openOrder2 = new orderModel({
       orderNumber: id4,
       company: "Ace Construction",
       dateStart: "02/02/2024",
@@ -91,8 +90,8 @@ async function populateOrders() {
       deliveryPostCode: "FV31DA",
       contactPhone: 447390796360,
       contactEmail: "ace.construction@email.com",
-    };
-    const openOrder3 = {
+    });
+    const openOrder3 = new orderModel({
       orderNumber: id5,
       company: "Colossal Industries",
       dateStart: "25/01/2024",
@@ -107,8 +106,8 @@ async function populateOrders() {
       deliveryPostCode: "BT21RT",
       contactPhone: 445612425468,
       contactEmail: "colossal.industries@email.com",
-    };
-    const openOrder4 = {
+    });
+    const openOrder4 = new orderModel({
       orderNumber: id6,
       company: "Ace Construction",
       dateStart: "14/01/2024",
@@ -123,12 +122,63 @@ async function populateOrders() {
       deliveryPostCode: "FV31DA",
       contactPhone: 447390796360,
       contactEmail: "ace.construction@email.com",
-    };
+    });
 
     //----------------------------------------------------> Check that my default orders are there and replace/insert them
-    await productModel.deleteMany({});
+    if (!exists1) {
+      await closedOrder1.save();
+    } else
+      orderModel.updateOne(
+        { orderNumber: id1 },
+        { $set: closedOrder1.toObject() }
+      );
+
+    if (!exists2) {
+      await closedOrder2.save();
+    } else {
+      await orderModel.updateOne(
+        { orderNumber: id2 },
+        { $set: closedOrder2.toObject() }
+      );
+    }
+
+    if (!exists3) {
+      await openOrder1.save();
+    } else {
+      await orderModel.updateOne(
+        { orderNumber: id3 },
+        { $set: openOrder1.toObject() }
+      );
+    }
+
+    if (!exists4) {
+      await openOrder2.save();
+    } else {
+      await orderModel.updateOne(
+        { orderNumber: id4 },
+        { $set: openOrder2.toObject() }
+      );
+    }
+
+    if (!exists5) {
+      await openOrder3.save();
+    } else {
+      await orderModel.updateOne(
+        { orderNumber: id5 },
+        { $set: openOrder3.toObject() }
+      );
+    }
+
+    if (!exists6) {
+      await openOrder4.save();
+    } else {
+      await orderModel.updateOne(
+        { orderNumber: id6 },
+        { $set: openOrder4.toObject() }
+      );
+    }
   } catch (error) {
     console.error("Error populating the products in the database:", error);
   }
 }
-module.exports = populateProducts;
+module.exports = populateOrders;
