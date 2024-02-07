91d2d1e101d7b1050a68caf5e2c6eb55301ba145
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-07 15:23:31 +0000

    Added error handlign and loading for outstanding orders.

--- ww-react-app/src/components/dashboard/tab-content/ManageOutstandingOrderContent.jsx
+++ ww-react-app/src/components/dashboard/tab-content/ManageOutstandingOrderContent.jsx
@@ -13,10 +13,15 @@ import {
 } from "@mui/material";
 import CircleProgressBar from "../../basicUI/CircleProgressBar";
 import getOpenOrders from "../../../functions/order_functions/getOpenOrders";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function ManageOutstandingOrderContent() {
   const theme = useTheme();
   const [orders, setOrders] = useState([]);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const cellStyle = {
     borderRight: "1px solid rgba(224, 224, 224, 1)",
@@ -24,114 +29,167 @@ function ManageOutstandingOrderContent() {
 
   useEffect(() => {
     const fetchOrders = async () => {
-      const openOrders = await getOpenOrders();
-      setOrders(openOrders);
+      try {
+        const openOrders = await getOpenOrders();
+
+        if (openOrders.length === 0) {
+          setIsEmpty(true);
+        } else {
+          setOrders(openOrders);
+          setIsEmpty(false);
+          setIsError(false);
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setIsEmpty(true);
+          setIsLoading(false);
+        } else {
+          console.error("Failed to fetch all orders:", error);
+          setIsError(true);
+          setIsLoading(false);
+        }
+      }
     };
 
     fetchOrders();
   }, []);
 
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Typography variant="h3" style={{ marginBottom: "20px" }}>
+          Loading...
+        </Typography>
+        <FontAwesomeIcon icon={faSpinner} spin size="4x" />
+      </div>
+    );
+  }
+
   return (
     <>
       <Typography variant="h3"> Manage Outstanding Orders </Typography>
-      <TableContainer component={Paper}>
-        <Table
-          style={{
-            width: "100%",
-          }}
-        >
-          <TableHead>
-            <TableRow>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                Order Number
-              </TableCell>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                Company
-              </TableCell>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                Date
-              </TableCell>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                Product Type
-              </TableCell>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                Remaining
-              </TableCell>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                Order Progress
-              </TableCell>
-              <TableCell
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                }}
-              >
-                View/Edit
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orders.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell sx={{ ...cellStyle }}>{row.orderNumber}</TableCell>
-                <TableCell sx={{ ...cellStyle }}>{row.company}</TableCell>
-                <TableCell sx={{ ...cellStyle }}>{row.dateStart}</TableCell>
-                <TableCell sx={{ ...cellStyle }}>{row.product}</TableCell>
-                <TableCell sx={{ ...cellStyle }}>
-                  {row.quantity - row.amountDelivered + " kg"}
-                </TableCell>
-                <TableCell sx={{ ...cellStyle }}>
-                  <CircleProgressBar
-                    value={(row.amountDelivered / row.quantity) * 100}
-                  />
-                </TableCell>
-
-                <TableCell>
-                  <Button
+      {isEmpty === true ? (
+        <div>There are no Orders on the system yet.</div>
+      ) : null}
+      {isError === true ? (
+        <div>
+          Something went wrong fetching Open Orders from the server. Please
+          contact the administrator.
+        </div>
+      ) : null}
+      {isEmpty === false && isError === false ? (
+        <>
+          <TableContainer component={Paper}>
+            <Table
+              style={{
+                width: "100%",
+              }}
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell
                     sx={{
-                      backgroundColor: theme.palette.accent.main,
-                      color: "black",
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
                     }}
-                    onClick={() => handleCancel(Reg)}
+                  >
+                    Order Number
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
+                    }}
+                  >
+                    Company
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
+                    }}
+                  >
+                    Date
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
+                    }}
+                  >
+                    Product Type
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
+                    }}
+                  >
+                    Remaining
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
+                    }}
+                  >
+                    Order Progress
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.contrastText,
+                    }}
                   >
                     View/Edit
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {orders.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell sx={{ ...cellStyle }}>
+                      {row.orderNumber}
+                    </TableCell>
+                    <TableCell sx={{ ...cellStyle }}>{row.company}</TableCell>
+                    <TableCell sx={{ ...cellStyle }}>{row.dateStart}</TableCell>
+                    <TableCell sx={{ ...cellStyle }}>{row.product}</TableCell>
+                    <TableCell sx={{ ...cellStyle }}>
+                      {row.quantity - row.amountDelivered + " kg"}
+                    </TableCell>
+                    <TableCell sx={{ ...cellStyle }}>
+                      <CircleProgressBar
+                        value={(row.amountDelivered / row.quantity) * 100}
+                      />
+                    </TableCell>
+
+                    <TableCell>
+                      <Button
+                        sx={{
+                          backgroundColor: theme.palette.accent.main,
+                          color: "black",
+                        }}
+                        onClick={() => handleCancel(Reg)}
+                      >
+                        View/Edit
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      ) : null}
     </>
   );
 }
