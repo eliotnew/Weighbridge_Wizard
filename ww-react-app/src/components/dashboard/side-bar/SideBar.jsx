d93f3fb8a8a67ec4018e6dbc62526a2fb3ccff46
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-11 14:22:29 +0000

    Made a create a truck but gvw doesnt work with server

--- ww-react-app/src/components/dashboard/side-bar/SideBar.jsx
+++ ww-react-app/src/components/dashboard/side-bar/SideBar.jsx
@@ -14,6 +14,7 @@ import OutstandingOrderButton from "./nav-buttons/OutstandingOrderButton";
 import TrainingButton from "./nav-buttons/TrainingButton";
 import ViewTicketsButton from "./nav-buttons/ViewTicketsButton";
 import TruckDatabaseButton from "./nav-buttons/TruckDatabase";
+import AddTruckButton from "./nav-buttons/AddTruck";
 
 function SideBar({ addTab }) {
   const theme = useTheme();
@@ -75,6 +76,7 @@ function SideBar({ addTab }) {
             Manage Trucks
           </Typography>
           <Divider />
+          <AddTruckButton addTab={addTab} />
           <TruckDatabaseButton addTab={addTab} />
           <ViewTicketsButton addTab={addTab} />
 
---
+++ ww-react-app/src/components/dashboard/side-bar/nav-buttons/AddTruck.jsx
@@ -0,0 +1,41 @@
+import { useTheme } from "@mui/material";
+import { ListItemButton, ListItemText } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTruckMedical } from "@fortawesome/free-solid-svg-icons";
+import TruckDatabaseContent from "../../tab-content/TruckDatabaseContent";
+import AddTruckContent from "../../tab-content/AddTruckContent";
+function AddTruckButton({ addTab }) {
+  const theme = useTheme();
+  const handleClick = () => {
+    const id = Math.floor(Math.random() * 100000);
+    const newTab = {
+      id: id,
+      label: "Add Truck",
+      content: <AddTruckContent />,
+    };
+    addTab(newTab);
+  };
+  return (
+    <ListItemButton
+      sx={{
+        "&:hover": {
+          color: theme.palette.secondary.contrastText,
+          transform: "scale(1.1)",
+          transition: "transform 0.35s",
+        },
+      }}
+      onClick={handleClick}
+    >
+      <FontAwesomeIcon
+        icon={faTruckMedical}
+        size="lg"
+        style={{
+          color: theme.palette.primary.contrastText,
+          paddingRight: "10px",
+        }}
+      />
+      <ListItemText primary="Add Truck" />
+    </ListItemButton>
+  );
+}
+export default AddTruckButton;
