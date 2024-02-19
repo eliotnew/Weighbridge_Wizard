307432193c75331055839ffec229fefbf39d233b
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-19 13:26:41 +0000

    Put annotations and images in folders

---
+++ ww-AI/ObjectDetection/trainYolo.py
@@ -0,0 +1,4 @@
+# This code will train an object detection model from the images found in "./Images" and the Annotations found in "./Annotations". I annotated using "makesense.ai".
+# I will use Yolo v8s because of it's lightweight and fast object detection ability.
+# Detected objects will be used to then export and identify text values within the number plates.
+
