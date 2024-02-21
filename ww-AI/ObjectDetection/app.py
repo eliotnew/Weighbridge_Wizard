3c07b28a9749e7d11a96ae1c96a7d34d1480875c
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-21 14:58:28 +0000

    app.py now runs (without docker) and the hello world works

--- ww-AI/ObjectDetection/app.py
+++ ww-AI/ObjectDetection/app.py
@@ -1,20 +1,26 @@
-import torch
 from flask import Flask, request, jsonify
 from PIL import Image
 from torchvision import transforms
 import pytesseract
 from flask_cors import CORS
+from ultralytics import YOLO
 
 app = Flask(__name__)
 CORS(app)
 
 # Load my custom YOLOv8 model ( i will need to retrain on actual data taken with my webcam.)
-model = torch.hub.load('ultralytics/yolov8', 'custom', path='./runs/detect/train/weights/best.pt', source='local').eval()
+#model = torch.hub.load('ultralytics/yolov8', 'custom', path='./runs/detect/train/weights/best.pt', source='local').eval()
+model = YOLO('./runs/detect/train/weights/best.pt')
 
 
 transform = transforms.Compose([
     transforms.ToTensor(),
 ])
+
+@app.route('/test', methods=['GET'])
+def helloWorld():
+    return "Hello World", 200
+
 
 @app.route('/reg', methods=['POST'])
 def upload_image():
@@ -56,4 +62,5 @@ def extractRegText(images):
     return text.strip()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
