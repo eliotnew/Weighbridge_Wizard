292e5d095c4a22e675fc30e1503f52956f46581e
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-02-26 17:02:36 +0000

    Reg is stored in a state

--- ww-react-app/src/components/camera/BasicWebcam.jsx
+++ ww-react-app/src/components/camera/BasicWebcam.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 
 function BasicWebcam() {
+  const [reg, setReg] = useState("");
   const webcamRef = useRef(null);
 
   const captureAndSend = React.useCallback(() => {
     if (webcamRef.current) {
       console.log("webcam is current!");
       const imageSrc = webcamRef.current.getScreenshot();
-      const blob = dataURItoBlob(imageSrc); // Convert base64 to blob
+      const blob = dataURItoBlob(imageSrc); // Convert base64 to blob for url transport
       const formData = new FormData();
       formData.append("image", blob, "image.jpg");
 
@@ -19,6 +20,23 @@ function BasicWebcam() {
         .then((response) => response.json())
         .then((data) => {
           console.log("Success:", data.extracted_texts);
+
+          // Check if extracted_texts is an array with multiple entries
+          if (
+            Array.isArray(data.extracted_texts) &&
+            data.extracted_texts.length > 1
+          ) {
+            setReg("Multiple registrations detected.");
+          } else if (
+            Array.isArray(data.extracted_texts) &&
+            data.extracted_texts.length === 1
+          ) {
+            // If it's an array with a single entry, use that entry
+            setReg(data.extracted_texts[0]);
+          } else {
+            // If it's not an array or is empty, set a default message
+            setReg("No reg detected");
+          }
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -42,12 +60,16 @@ function BasicWebcam() {
   useEffect(() => {
     const interval = setInterval(() => {
       captureAndSend();
-      console.log("timer ticked");
-    }, 3000); // Set to capture every 1.5 seconds
+    }, 3000); // Set to capture every 3 secs
 
     // Cleanup function to clear the interval
     return () => clearInterval(interval);
   }, [captureAndSend]);
+
+  // Use useEffect to log the updated state
+  useEffect(() => {
+    console.log("Updated reg is: " + reg);
+  }, [reg]);
   return (
     <div
       style={{
