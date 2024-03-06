08d8d990a917723f781d70823783ab855e89b4d0
Eliot New <eliot.new@students.plymouth.ac.uk>
2024-03-06 10:33:18 +0000

    Finished tests on the orders in the express app

--- ww-express-app/tests/integration/orders.test.js
+++ ww-express-app/tests/integration/orders.test.js
@@ -123,11 +123,87 @@ describe("Order Testing", async () => {
 
     expect(closeRes).to.have.status(200);
     expect(closeRes.body).to.have.property("message", "Closed Order");
+  });
 
-    // const orderIsOpen = fetchRes.body.some(
-    //   (order) =>
-    //     order.orderNumber === createdOrderNumber && order.open === false
-    // );
-    // expect(orderIsOpen).to.be.false;
+  it("should successfully get closed orders", async () => {
+    // Step 1: arrange
+    let res = await chai
+      .request(server)
+      .post("/order/create")
+      .send(exampleOrder);
+
+    expect(res).to.have.status(201);
+    expect(res.body).to.have.property("message", "Order Created Successfully");
+
+    res = await chai.request(server).get("/order/getopen");
+
+    let orderNumber = res.body[0]?.orderNumber;
+
+    console.log("-----------------------" + orderNumber);
+
+    const closeRes = await chai
+      .request(server)
+      .put("/order/close")
+      .send({ orderNumber: orderNumber });
+
+    expect(closeRes).to.have.status(200);
+    expect(closeRes.body).to.have.property("message", "Closed Order");
+
+    // Step 2: getclosed orders
+
+    const closedArrayRes = await chai.request(server).get("/order/getclosed");
+
+    //assert
+    expect(closedArrayRes).to.have.status(200);
+    expect(closedArrayRes.body).to.be.an("array");
+    let closedOrderNumber = closedArrayRes.body[0]?.orderNumber;
+    expect(closedOrderNumber).to.equal(orderNumber);
+  });
+
+  it("should correctly inform the client that the order number was not found when closing order", async () => {
+    // Step 2: act  close the created order
+    const closeRes = await chai
+      .request(server)
+      .put("/order/close")
+      .send({ orderNumber: "ORDR0000000" });
+
+    expect(closeRes).to.have.status(404);
+    expect(closeRes.body).to.have.property(
+      "message",
+      "The order does not exist on the db."
+    );
+  });
+
+  it("should correctly inform the client that there are no open orders that match the truck type", async () => {
+    // Step 2: act  close the created order
+    const closeRes = await chai
+      .request(server)
+      .get("/order/getcompatible")
+      .query({ compatible: "hotbox" });
+
+    expect(closeRes).to.have.status(404);
+    expect(closeRes.body).to.have.property(
+      "message",
+      "No orders for this truck type (hotbox)are currently available."
+    );
+  });
+
+  it("should successfully inform the client that there is a compatible order by trucktype", async () => {
+    const res = await chai
+      .request(server)
+      .post("/order/create")
+      .send(exampleOrder);
+
+    expect(res).to.have.status(201);
+
+    expect(res.body).to.have.property("message", "Order Created Successfully");
+
+    // Step 2: act  close the created order
+    const closeRes = await chai
+      .request(server)
+      .get("/order/getcompatible")
+      .query({ compatible: "dump" });
+
+    expect(closeRes).to.have.status(200);
   });
 });
